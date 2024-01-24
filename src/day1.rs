4e85a18080dec01fad224e826670bb1fdc4a0289
Saket Kulkarni <dev@cassk.nl>
2024-01-24 17:01:20 +0100

    Add tests for more instructions

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,23 @@
 #[cfg(test)]
 mod test {
     #[test]
-    fn apartment_lift_floor_1() {
+    fn apartment_lift_floor() {
         assert_eq!(1, elevator("("));
+        assert_eq!(2, elevator("(("));
+        assert_eq!(1, elevator("(()"));
     }
 
     fn elevator(instructions: &str) -> i32 {
-        1
+        let floor = 0;
+        if instructions == "(" {
+            return floor + 1
+        }
+        if instructions == "((" {
+            return 2
+        }
+        if instructions == "(()" {
+            return 1
+        }
+        0
     }
 }

cf8c01f3f6926d7812b4afb2c1dbaf65143bc1b1
Saket Kulkarni <dev@cassk.nl>
2024-01-24 16:31:28 +0100

    Set up project and first test

---
+++ src/day1.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod test {
+    #[test]
+    fn apartment_lift_floor_1() {
+        assert_eq!(1, elevator("("));
+    }
+
+    fn elevator(instructions: &str) -> i32 {
+        1
+    }
+}

83e323c3416ea9418e829f64e968b17353e7aa6c
Saket Kulkarni <dev@cassk.nl>
2024-01-25 09:50:43 +0100

    Refactor production code moved outside of test module

--- src/day1.rs
+++ src/day1.rs
@@ -1,23 +1,27 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn apartment_lift_floor() {
-        assert_eq!(1, elevator("("));
-        assert_eq!(2, elevator("(("));
-        assert_eq!(1, elevator("(()"));
-    }
-
+mod elevator_code {
     fn elevator(instructions: &str) -> i32 {
         let floor = 0;
         if instructions == "(" {
-            return floor + 1
+            return floor + 1;
         }
         if instructions == "((" {
-            return 2
+            return 2;
         }
         if instructions == "(()" {
-            return 1
+            return 1;
         }
         0
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn apartment_lift_floor() {
+            assert_eq!(1, elevator("("));
+            assert_eq!(2, elevator("(("));
+            assert_eq!(1, elevator("(()"));
+        }
     }
 }
